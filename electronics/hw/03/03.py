265b22dd2d0ead8a66512c8e73554093a81c354f
Ben Kallus <bkallus@hamilton.edu>
2021-02-19 14:47:31 -0500

    electronics hw 3

---
+++ electronics/hw/03/03.py
@@ -0,0 +1,1 @@
+print((n:=float(input("Enter a number: ")), m:=float(input("Enter a number: ")),"")[2]+"\n".join(f"{n} {o} {m} = {eval(f'{n}{o}{m}')}"for o in{'-','+','*','**'}|({'/','%'}if m else set())))
