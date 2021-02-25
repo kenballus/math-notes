cb99d807814b8a1553d40627fb451875028cda97
bkallus@hamilton.edu <bkallus@hamilton.edu>
2021-02-24 23:49:37 -0500

    a lot more

--- electronics/hw/03/03.py
+++ electronics/hw/03/03.py
@@ -1,1 +1,4 @@
+# Author: Ben Kallus
+# Asks for two numbers, then prints their sum, difference, product, the result of raising the first to the power of the second. Also prints their quotient and remainder, if the second number is not 0.
+
 print((n:=float(input("Enter a number: ")),m:=float(input("Enter a number: ")),"")[2]+"\n".join(f"{n} {o} {m} = {eval(f'{n}{o}{m}')}"for o in{'-','+','*','**'}|({'/','%'}if m else set())))
