b7f3af14f5015ed096aa41c7876c37a7bd86db89
bkallus@hamilton.edu <bkallus@hamilton.edu>
2021-03-04 09:57:46 -0500

    electronics labs

--- electronics/labs/08/plot_points.py
+++ electronics/labs/08/plot_points.py
@@ -2,13 +2,14 @@ from matplotlib import pyplot as plt
 import math
 import pandas as pd
 
-mA = [0, 10, 50, 100, 150, 200, 250, 300, 350, 400, 450]
-V = [0,0.67,0.77,0.80,0.81,0.82,0.83,0.83,0.84,0.84,0.84]
+df = pd.read_csv("data1.csv")
+V = df['V']
+mA = df['mA']
 
 # plt.xscale("log")
 # plt.yscale("log")
 
-plt.plot(V, mA, marker='o')
+plt.plot(V, mA, marker='o', linewidth=0)
 plt.xlabel('V', fontsize=14)
 plt.ylabel('mA', fontsize=14)
 plt.grid(True)
