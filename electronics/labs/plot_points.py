be17091d3d0e6ef35eea41b5a31659efa98cc3a2
Ben Kallus <bkallus@hamilton.edu>
2021-03-24 00:36:12 -0400

    graph theory hw 9 done for now

--- electronics/labs/plot_points.py
+++ electronics/labs/plot_points.py
@@ -3,15 +3,14 @@ import math
 import pandas as pd
 
 df = pd.read_csv("data.csv")
-f = df['f']
-vin = df['vin']
-vout = df['vout']
+x = df['x_label']
+y = df['y_label']
 
 # plt.xscale("log")
 # plt.yscale("log")
 
-plt.plot(f, [vout[i]/vin[i] for i in range(len(vin))], marker='o')
-plt.xlabel('Frequency (Hz)', fontsize=14)
-plt.ylabel('Phase difference (°) ', fontsize=14)
+plt.plot(x, y) # marker='o' will make dots on the graph, linewidth=0 will not draw a line between the dots
+plt.xlabel('The X Axis', fontsize=14)
+plt.ylabel('The Y Axis', fontsize=14)
 plt.grid(True)
 plt.show()
